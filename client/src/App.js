37df6ee3b778af8631d2ad7e3598a19a09701800
Alper Canberk <alpercanberk@MacBook-Pro-6.local>
2018-07-07 12:21:15 +0300

    more fixes

--- client/src/App.js
+++ client/src/App.js
@@ -107,7 +107,7 @@ class App extends Component {
               component={() => {
                 axios
                   .get("/logout")
-                  .then((window.location = "/"));
+                  .then((window.location = "reshwap.com"));
               }}
             />
           </div>
